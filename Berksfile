45393425becd8a395eef0af849c7e32c397a0e7d
RMcNairn <rmcnairn@gmail.com>
2014-05-14 14:24:02 +0100

    added additional config for NGINX ruby and puma

--- Berksfile
+++ Berksfile
@@ -1,4 +1,6 @@
-source "https://api.berkshelf.com"
+site :opscode
 
 cookbook 'nginx', '~> 2.0'
+cookbook 'ruby_build', github: 'fnichol/chef-ruby_build'
+cookbook 'rbenv', github: 'fnichol/chef-rbenv'
 cookbook 'dorsai', path: 'cookbooks/dorsai'
--- cookbooks/dorsai/recipes/default.rb
+++ cookbooks/dorsai/recipes/default.rb
@@ -2,8 +2,18 @@
 # Cookbook Name:: dorsai
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
+# Copyright 2014, dorsai
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe 'ruby_build'
+include_recipe 'rbenv::system'
 include_recipe 'nginx'
+
+template "/etc/nginx/sites-available/#{node['dorsai']['app_name']}" do
+  source 'dorsai.erb'
+end
+
+nginx_site node['dorsai']['app_name'] do
+  enable true
+end
